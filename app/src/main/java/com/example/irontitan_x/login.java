eb178dc8a95217674415fbdbd69bdfd3f1632287
adhamzorkani <adham.zorkani@gmail.com>
2024-07-04 10:37:13 +0300

    finished login and sign up added a new activity

--- app/src/main/java/com/example/irontitan_x/login.java
+++ app/src/main/java/com/example/irontitan_x/login.java
@@ -1,6 +1,10 @@
 package com.example.irontitan_x;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class login extends AppCompatActivity {
+
+    Button loginBtn;
+    TextView signUpTV, forgotPasswordTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,36 @@ public class login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        loginBtn=findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, signUp.class); //should be main screen
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        signUpTV=findViewById(R.id.signUpButton);
+        signUpTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, signUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        forgotPasswordTV=findViewById(R.id.forgotPasswordButton);
+        forgotPasswordTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, signUp.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
