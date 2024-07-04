7730e210726cfcab85828cd689d9da2ec0b9b019
aelzorkani <aelzorkani@getocto.io>
2024-07-04 17:45:08 +0300

    first push to branch

--- app/src/main/java/com/example/irontitan_x/goals2.java
+++ app/src/main/java/com/example/irontitan_x/goals2.java
@@ -1,6 +1,9 @@
 package com.example.irontitan_x;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class goals2 extends AppCompatActivity {
+
+    Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class goals2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        nextBtn = findViewById(R.id.nextButton);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(goals2.this, goals3.class);
+                startActivity(intent);
+            }
         });
     }
 }
