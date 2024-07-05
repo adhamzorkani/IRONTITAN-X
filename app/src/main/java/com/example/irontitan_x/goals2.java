32361ce8910c36069d156509fab91df72ed12f81
adhamzorkani <adham.zorkani@gmail.com>
2024-07-05 16:17:57 +0300

    finished all screens. still  need to finish activities

--- app/src/main/java/com/example/irontitan_x/goals2.java
+++ app/src/main/java/com/example/irontitan_x/goals2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class goals2 extends AppCompatActivity {
 
     Button nextBtn;
+    TextView backTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +36,14 @@ public class goals2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        backTV = findViewById(R.id.backText);
+        backTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/irontitan_x/goals4.java
+++ app/src/main/java/com/example/irontitan_x/goals4.java
@@ -1,6 +1,10 @@
 package com.example.irontitan_x;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class goals4 extends AppCompatActivity {
+    Button nextBtn;
+    TextView backTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,23 @@ public class goals4 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        nextBtn = findViewById(R.id.nextButton);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(goals4.this, goals3.class); //should go to home
+                startActivity(intent);
+            }
+        });
+
+        backTV = findViewById(R.id.backText);
+        backTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
