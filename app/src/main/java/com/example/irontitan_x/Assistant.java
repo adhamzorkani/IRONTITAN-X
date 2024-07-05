dafae93424946ca7f377e991f5941530e951f0db
adhamzorkani <adham.zorkani@gmail.com>
2024-07-06 00:58:27 +0300

    finished final touches on chatbot screen

--- app/src/main/java/com/example/irontitan_x/Assistant.java
+++ app/src/main/java/com/example/irontitan_x/Assistant.java
@@ -1,6 +1,10 @@
 package com.example.irontitan_x;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Assistant extends AppCompatActivity {
+
+    ImageButton backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,14 @@ public class Assistant extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        backBtn=findViewById(R.id.backButton);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/irontitan_x/Home.java
+++ app/src/main/java/com/example/irontitan_x/Home.java
@@ -1,6 +1,10 @@
 package com.example.irontitan_x;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
+
+    ImageButton chatbotBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,15 @@ public class Home extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        chatbotBtn = findViewById(R.id.chatBotButton);
+        chatbotBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Assistant.class);
+                startActivity(intent);
+            }
         });
     }
 }
