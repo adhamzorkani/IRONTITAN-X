01621fccdd260c296cff4687c0f3209ab2bd5759
adhamzorkani <adham.zorkani@gmail.com>
2024-07-06 01:03:38 +0300

    ..

--- app/src/main/java/com/example/irontitan_x/Assistant.java
+++ app/src/main/java/com/example/irontitan_x/Assistant.java
@@ -1,13 +1,9 @@
 package com.example.irontitan_x;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
 import android.widget.ImageButton;
 
-import androidx.activity.EdgeToEdge;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
